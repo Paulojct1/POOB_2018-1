2b5b3c717adeef4a9fd618f924dde4fc8982938e
Paulo José <38256436+Paulojct1@users.noreply.github.com>
2018-04-10 12:46:48 -0300

    Add files via upload

---
+++ caRetangulo/caRetangulo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  Bemvindo a POO!");
+            
+            // criando um objeto da classe Retangulo
+
+            Retangulo r1 = new Retangulo();
+            double area = r1.calculaArea();
+            double perimetro = r1.calculaPerimetro();
+            Console.WriteLine("  Area do r1: " + area);
+            Console.WriteLine("  Perimetro do r1: " + perimetro);
+
+            //r1.setBasis(4.0);
+            //r1.setHeight(5.0);
+
+            area = r1.calculaArea();
+            perimetro = r1.calculaPerimetro();
+
+            Console.WriteLine("  Area do r1: " + area);
+			Console.WriteLine("  Perimetro do r1: " + perimetro);
+
+			Retangulo r2 = new Retangulo(7.0, 8.0);
+			area = r2.calculaArea();
+			perimetro = r2.calculaPerimetro();
+
+			Console.WriteLine("  Area do r2: " + area);
+			Console.WriteLine("  Perimetro do r2: " + perimetro);
+
+			Retangulo r3 = new Retangulo("  Oi,", " tudo bem?");
+
+			Console.Read();
+        }
+    }
+}
---
+++ caRetangulo/caRetangulo/Retangulo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caRetangulo
+{
+    class Retangulo
+    {
+        // atributos
+
+        private double basis;
+        private double height;
+
+        // metodos
+
+		//// construtores de classe 
+
+		public Retangulo()
+		{
+			basis = 4.0;
+			height = 5.0;
+		}
+
+		public Retangulo(double b, double h)
+		{
+			basis = b;
+			height = h;
+		}
+
+		public Retangulo(String a, String b)
+		{
+			Console.WriteLine(a + b);
+		}
+		
+        public double calculaArea()
+        {
+            return (basis * height);
+        }
+
+        // getters() e setter()
+
+        public void setBasis(double b)
+        {
+            basis = b;
+        }
+
+        public void setHeight(double h)
+        {
+            height = h;
+        }
+
+        public double calculaPerimetro()
+        {
+            return (2 * basis + 2 * height);
+        }
+
+    } // fim da classe Retangulo
+}
