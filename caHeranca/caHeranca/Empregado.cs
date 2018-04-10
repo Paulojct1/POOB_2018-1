4dc093f2f1b6b0ed67ebe1d5650f7e5a7e1f99c5
Paulo José <38256436+Paulojct1@users.noreply.github.com>
2018-04-10 12:56:08 -0300

    Add files via upload

---
+++ caHeranca/caHeranca/Empregado.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caHeranca
+{
+	class Empregado
+	{
+		// Atributos
+
+		protected String nome;
+		protected String secao;
+		protected double salario;
+
+		// Metodos
+
+		public Empregado()
+		{
+
+		}
+
+		public Empregado(String _nome, String _secao, double _salario)
+		{
+			nome = _nome;
+			secao = _secao;
+			salario = _salario;
+		}
+
+		public String getNome()
+		{
+			return nome;
+		}
+
+		public void setNome(String _nome)
+		{
+			this.nome = _nome;
+		}
+
+		public String getSecao()
+		{
+			return secao;
+		}
+
+		public void setSecao(String _secao)
+		{
+			this.secao = _secao;
+		}
+
+		public double getSalario()
+		{
+			return salario;
+		}
+
+		public void setSalario(double _salario)
+		{
+			this.salario = _salario;
+		}
+
+		public virtual void aumentaSalario(double percentual)
+		{
+			salario *= 1 + percentual / 100;
+			// salario = salario * (1 + percentual / 100)
+		}
+		
+		// Fim da classe Empregado
+	}
+}
---
+++ caHeranca/caHeranca/Gerente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caHeranca
+{
+	class Gerente : Empregado  // Gerente herda de Empregado
+	{
+		// Atributos
+
+		private String secretaria;
+
+		// Metodos
+
+		public Gerente()
+		{
+
+		}
+
+		public Gerente(String _nome, String _secao, double _salario, String _secretaria) : base(_nome, _secao, _salario, _secretaria)
+		{
+			secretaria = _secretaria;
+		}
+
+		public String getSecretaria()
+		{
+			return (secretaria);
+		}
+
+		public void setSecretaria(String _secretaria)
+		{
+			this.secretaria = _secretaria;
+		}
+
+		public override void aumentaSalario(double percentagem)
+		{
+			double novePercentagem = percentagem + 15.0;
+			base.aumentaSalario
+		}
+	}
+}
