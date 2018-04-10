cd87955028cedc2658976ab525a881fcd192c37a
Paulo José <38256436+Paulojct1@users.noreply.github.com>
2018-04-10 12:45:32 -0300

    Add files via upload

---
+++ caNumeroComplexo/caNumeroComplexo/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNumeroComplexo
+{
+	class NumComplexo
+	{
+		
+		// atributos
+
+		private double re;
+		private double im;
+
+		// construtores
+
+		public NumComplexo() // default
+		{
+			re = 0.0;
+			im = 0.0;
+		}
+
+		public NumComplexo(double a, double b)
+		{
+			re = a;
+			im = b;
+		}
+
+		public double modulo()
+		{
+			double modulo;
+			modulo = Math.Sqrt(Math.Pow(re, 2) + Math.Pow(im, 2));
+
+			return (modulo);
+		}
+
+		public NumComplexo soma()
+		{
+			NumComplexo zx = new NumComplexo()
+			{
+				
+				return zx;
+			}
+		}
+
+		public double print()
+		{
+			Console.WriteLine("  O numero complexo z3 eh: " + ;
+		}
+	}
+}
+
+
+
+
+
+
---
+++ caNumeroComplexo/caNumeroComplexo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNumeroComplexo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			NumComplexo z1 = new NumComplexo(3.0, 4.0);
+
+			Console.WriteLine("  O modulo do r1 eh: " + r1.modulo());
+
+			NumComplexo z2 = new NumComplexo(5.7, 6.8);
+			NumComplexo z3 = new NumComplexo();
+
+			z3 = z1.soma(z2);
+
+			z3.print();
+
+			Console.Read();
+		}
+	}
+}
