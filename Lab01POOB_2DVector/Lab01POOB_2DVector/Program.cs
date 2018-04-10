ec52b140952bd923a87888dae2b2e66dca68b564
Paulo José <38256436+Paulojct1@users.noreply.github.com>
2018-04-10 12:47:51 -0300

    Add files via upload

---
+++ Lab01POOB_2DVector/Lab01POOB_2DVector/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01POOB_2DVector
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector vect1 = new Vector();
+			Vector vect2 = new Vector();
+			Vector projecao = new Vector();
+
+			Console.WriteLine("\n  Digite um valor para vect1(_, ): ");
+			double a1 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("  Digite um valor para vect1( ,_): ");
+			double b1 = Convert.ToDouble(Console.ReadLine());
+
+			vect1.setA(a1);
+			vect1.setB(b1);
+
+			Console.WriteLine("\n  Digite um valor para vect2(_, ): ");
+			double a2 = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("  Digite um valor para vect2( ,_):");
+			double b2 = Convert.ToDouble(Console.ReadLine());
+
+			vect2.setA(a2);
+			vect2.setB(b2);
+
+			Console.WriteLine("\n  O modulo do Vector 1 é: " + vect1.modulo());
+			Console.WriteLine("  O modulo do Vector 2 é: " + vect2.modulo());
+
+			Console.WriteLine("\n  Calculando o Produto Escalar");
+			Console.WriteLine("  Produto Escalar: " + vect1.prod_escalar(a2, b2) + "\n");
+
+			Console.WriteLine("\n  Calculando o Angulo entre o Vector 1 e o Vector 2");
+			Console.WriteLine("  Angulo entre Vector 1 e o Vector 2: " + vect1.angle(a2, b2));
+
+			projecao = vect1.projecao(vect2);
+			Console.WriteLine("\n  O Valor da Projeção de Vector 1 em Vector 2 eh: (" + projecao.a1 + " ; " + projecao.b1 + ").");
+
+			Console.Read();
+		}
+	}
+}
---
+++ Lab01POOB_2DVector/Lab01POOB_2DVector/Vector.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01POOB_2DVector
+{
+	class Vector
+	{
+		// Declaração de Variáveis
+
+		public double a1;
+		public double b1;
+
+
+		//  Declaração do Vetor Vazio
+
+		public Vector()
+		{
+
+		}
+
+
+		//  Declaração do Vetor a receber variáveis
+
+		public Vector(double a1, double b1)
+		{
+			this.a1 = a1;
+			this.b1 = b1;
+		}
+
+
+		// Setter e Getters
+
+		public void setA(double a1)
+		{
+			this.a1 = a1;
+		}
+
+		public void setB(double b1)
+		{
+			this.b1 = b1;
+		}
+
+		public double getA()
+		{
+			return a1;
+		}
+
+		public double getB()
+		{
+			return b1;
+		}
+
+
+		// Função de retorno produto escalar
+
+		public double prod_escalar(double a2, double b2)
+		{
+			double f1 = a1 * a2;
+			double f2 = b1 * b2;
+			return (f1 + f2);
+		}
+
+
+		// Função de retorno do módulo
+
+		public double modulo()
+		{
+			return Math.Sqrt((a1 * a1) + (b1 * b1));
+		}
+
+
+		// Função de retorno do módulo para 2 entradas
+
+		public double modulo_2Ents(double a2, double b2)
+		{
+			return Math.Sqrt((a2 * a2) + (b2 * b2));
+		}
+
+
+		// Função de retorno do ângulo dos vetores
+
+		public double angle(double a2, double b2)
+		{
+			double produto_escalar = this.prod_escalar(a2, b2);
+			double modulo_Vector1 = modulo();
+			double modulo_Vector2 = modulo_2Ents(a2, b2);
+			double retorno = Math.Acos((produto_escalar) / (modulo_Vector1 * modulo_Vector2));
+			return retorno;
+		}
+
+
+		// Função de retorno do valor do Vetor 1 no Vetor 2
+
+		public Vector projecao(Vector entrada)
+		{
+			Vector projVector = new Vector();
+			double produto_escalar = this.prod_escalar(entrada.a1, entrada.b1);
+			double denominador = modulo_2Ents(entrada.a1, entrada.b1);
+			denominador = denominador * denominador;
+			double divisao = produto_escalar / denominador;
+			double a1_resultado = divisao * entrada.a1;
+			double b1_resultado = divisao * entrada.b1;
+			projVector.setA(a1_resultado);
+			projVector.setB(b1_resultado);
+			return projVector;
+		}
+	}
+}
